fba48a1baa51f9670bbf96e922bbb92c94721ead
Animesh Ramesh <animeshramesh@gmail.com>
2020-01-02 18:26:54 -0800

    Update med_container_most_water.py

--- Python/Two_Pointer/med_container_most_water.py
+++ Python/Two_Pointer/med_container_most_water.py
@@ -1,8 +1,13 @@
-def maxArea(self, height):
-    L, R, width, res = 0, len(height) - 1, len(height) - 1, 0
-    for w in range(width, 0, -1):
-        if height[L] < height[R]:
-            res, L = max(res, height[L] * w), L + 1
+def maxArea(self, height: List[int]) -> int:
+    left, right = 0, len(height) - 1
+    max_area = 0
+    while left < right:
+        l_h, r_h = height[left], height[right]
+        area = min(l_h, r_h) * (right - left)
+        max_area = max(max_area, area)
+        if l_h < r_h:
+            left += 1
         else:
-            res, R = max(res, height[R] * w), R - 1
-    return res
+            right -= 1
+
+    return max_area
